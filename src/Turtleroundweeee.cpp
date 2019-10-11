a4153f179e2efb47e0c975a5a6c0ba07600855c7
Jacob W. Knudsen <jknuds19@student.aau.dk>
2019-10-11 08:53:23 +0200

    Initial Roundround upload

--- src/Turtleroundweeee.cpp
+++ src/Turtleroundweeee.cpp
@@ -3,22 +3,22 @@
 
 int main(int argc, char *argv[])
 {
-    ros::init(argc, argv, "roundround");
+    ros::init(argc, argv, "Turtleroundweeee");
 
     ros::NodeHandle n;
     ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1);
 
     geometry_msgs::Twist cmd_vel_message;
-    cmd_vel_message.angular.z = 0.0001;
-    cmd_vel_message.linear.x = 5; 
+    cmd_vel_message.angular.z = 1;
+    cmd_vel_message.linear.x = 3; 
 
     ros::Rate loop_rate(10);
     while(ros::ok())
     {
         cmd_vel_pub.publish(cmd_vel_message);
         loop_rate.sleep();
-        cmd_vel_message.angular.z = cmd_vel_message.angular.z + 0.01;
-        cmd_vel_message.linear.x = cmd_vel_message.linear.x - 0.02;
+        cmd_vel_message.angular.z = cmd_vel_message.angular.z;
+        cmd_vel_message.linear.x = cmd_vel_message.linear.x;
     }
     return 0;
 }
