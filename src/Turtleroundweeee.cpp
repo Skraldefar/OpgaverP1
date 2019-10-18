82d5a3eafa2a0d45e94cae1f65aba4aeb6b07189
Jacob Knudsen <jknuds19@student.aau.dk>
2019-10-18 20:04:09 +0200

    Virk plz

--- src/Turtleroundweeee.cpp
+++ src/Turtleroundweeee.cpp
@@ -6,11 +6,11 @@ int main(int argc, char *argv[])
     ros::init(argc, argv, "Turtleroundweeee");
 
     ros::NodeHandle n;
-    ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1);
+    ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1);
 
     geometry_msgs::Twist cmd_vel_message;
-    cmd_vel_message.angular.z = 1;
-    cmd_vel_message.linear.x = 2; 
+    cmd_vel_message.angular.z = 0.2;
+    cmd_vel_message.linear.x = 0.5; 
 
     ros::Rate loop_rate(10);
     while(ros::ok())
