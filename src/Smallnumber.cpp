d72639cafd8e2ddade381479243a80f51fc48f69
Jacob Knudsen <jknuds19@student.aau.dk>
2019-10-24 17:13:41 +0200

    Update

--- src/Smallnumber.cpp
+++ src/Smallnumber.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
+
+
+int Vectorsize {};
+std::vector<int> Numre (Vectorsize);
+
+int smallnumber()
+{
+    int ien {};
+    int ito {};
+    for(int i = 0; i < Numre.size()-1; i++)
+    {
+
+    for(int i = 0; i<Numre.size()-1; i++)
+    {
+        if(Numre[i] > Numre[1+i])
+        {
+            ien = Numre[i];
+            ito = Numre[1+i];
+
+            Numre[1+i] = ien;
+            Numre[i] = ito;
+        }
+    }
+        std::cout << Numre[0] << std::endl;
+    }
+    return Numre[0];
+}
+
+
 
 
 int main()
 {
+    int input {};
     
+   
+    std::cout << "size vector " << Numre.size() << std::endl;
+    std::cout << smallnumber() << std::endl;
+
+
+
+
+
 }
